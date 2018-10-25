8d506dfa5dcca5ce88134a4705dfa67f4a081a32
nabeelz <nabeelz@icenginc.com>
2018-10-24 17:40:57 -0700

    Initial commit, two projects

---
+++ SKT Due Date Emailer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKT_Due_Date_Emailer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
