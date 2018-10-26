1c52b7294c5283950ead5a50de44d232c5ab08d7
nabeelz <nabeelz@icenginc.com>
2018-10-26 14:51:43 -0700

    Should be at properly filtered lists now. Relevant objects. Now must present them, then color them.

--- PCB-SKT Due Date Emailer/Program.cs
+++ PCB-SKT Due Date Emailer/Program.cs
@@ -19,15 +19,16 @@ namespace PCB_SKT_Due_Date_Emailer
 			DateTime userTime = TimeZoneInfo.ConvertTimeFromUtc(today, timeInfo);
 			string date = userTime.Month.ToString().PadLeft(2, '0') + "/" + userTime.Day.ToString().PadLeft(2, '0') + "/" + userTime.Year + " " + userTime.ToString("HH:mm:ss tt");
 
-			List<List<string>> dataListReturn = getAllPCBList();
+			var dataListReturn = getAllPCBList();
+			var filtered_list = filter_list(dataListReturn);
 		}
 
-		static private List<List<string>> getAllPCBList()
+		static private List<PCB_entry> getAllPCBList()
 		{
 
-			List<List<string>> dataListReturn = new List<List<string>>();
+			List<PCB_entry> dataListReturn = new List<PCB_entry>();
 
-			string databaseLocation = "\\\\QA_SERVER\\QA Database\\QUALITY_Database_BE.mdb";
+			string databaseLocation = "\\\\ICEDATA_SERVER\\Log-Book\\BoardDesign_Assy_Database_BE.mdb";
 			OleDbConnection con = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + databaseLocation + ";");
 
 			//Console.Write("Board Inventory Database Location: {0}\n", databaseLocation);
@@ -39,38 +40,36 @@ namespace PCB_SKT_Due_Date_Emailer
 
 				if (con.State == ConnectionState.Open)
 				{
-					Console.Write("Quality Database Opened\n");
+					Console.Write("PCB Database Opened\n");
 
 					OleDbCommand cmd = new OleDbCommand();
 					cmd.Connection = con;
 
-					string statement = "SELECT * FROM [tbl_Equipment PM List] WHERE [PM Completed]=FALSE";
-
+					string statement = "SELECT * FROM [tblPCB_Jobs] WHERE [PCB PO Date] > #6/14/2018#";
 
 					cmd.CommandText = statement;
 
 					using (OleDbDataReader rdr = cmd.ExecuteReader())
 					{
-
+						Console.WriteLine("Collecting Information...");
 						while (rdr.Read())
 						{
 
 							if (rdr.FieldCount > 0)
 							{
 								//Console.Write("Executing...\n");
-								List<string> tempStringList = new List<string>();
+								PCB_entry entry = new PCB_entry();
 
-								tempStringList.Add(rdr["System Number"].ToString());
-								tempStringList.Add(rdr["Equipment List Category"].ToString());
-								tempStringList.Add(rdr["Serial No"].ToString());
-								tempStringList.Add(rdr["System Description"].ToString());
-								tempStringList.Add(rdr["PM Cycle (Months)"].ToString());
-								tempStringList.Add(rdr["Last PM Date"].ToString());
-								tempStringList.Add(rdr["Next Due Date"].ToString());
-								tempStringList.Add(rdr["PM Actual Date"].ToString());
-								tempStringList.Add(rdr["Comments"].ToString());
-
-								dataListReturn.Add(tempStringList);
+								entry.job_num = (rdr[0].ToString()); //job number
+								entry.qty_ordered = (rdr["Qty Ordered"].ToString());
+								entry.customer = (rdr["Customer - PCB"].ToString());
+								entry.vendor = (rdr["PCB Vendor"].ToString());
+								entry.due_date_string = (rdr["PCB Due Date"].ToString());
+								entry.date_in_string = (rdr["PCB Recv'd Date"].ToString());
+								entry.order_date_string = (rdr["PCB PO Date"].ToString());
+								entry.pcb_work_ext = (rdr["PCB_Work Ext"].ToString());
+								
+								dataListReturn.Add(entry);
 
 							}//End if
 
@@ -82,15 +81,81 @@ namespace PCB_SKT_Due_Date_Emailer
 				}//End if connection open
 
 				con.Close();
-
+				Console.WriteLine("Connection closed");
 			}
 			catch (Exception err)
 			{
-				Console.Write("ERROR Accessing Quality DB: {0}\n", err.Message);
+				Console.Write("ERROR Accessing BoardDesign_Assy_Database_BE: {0}\n", err.Message);
 			}
 
 			return dataListReturn;
 
 		}//End of getAllPMList
+
+		static private List<PCB_entry> filter_list(List<PCB_entry> input)
+		{
+			var now = DateTime.Now;
+
+			for (int i = 0; i < input.Count; i++)
+			{
+				var entry = input[i];
+				//populate the datetime field
+				entry.convert_dates(); //converts dates and saves conversion success status
+
+				if (entry.conversion[1])//conversion 2 is order_date, make sure its valid
+				{
+					if (entry.conversion[2]) //if date in exists (its already done)
+					{
+						input.RemoveAt(i);
+						i--;
+					}
+				}
+			}
+
+			return input;
+		}
+	}
+
+	class PCB_entry
+	{
+		public string due_date_string;
+		public string order_date_string;
+		public string date_in_string;
+		public string qty_ordered;
+		public string customer;
+		public string job_num;
+		public string vendor;
+		public string pcb_work_ext;
+
+
+		public DateTime due_date;
+		public DateTime order_date;
+		public DateTime date_in;
+
+		public bool[] conversion;
+
+		public void convert_dates()
+		{
+			var due_date_bool = convert_due_date();
+			var order_date_bool = convert_order_date();
+			var date_in_bool = convert_date_in();
+
+			conversion = new bool[] { due_date_bool, order_date_bool, date_in_bool };
+		}
+
+		private bool convert_date_in()
+		{
+			return DateTime.TryParse(date_in_string, out date_in);
+		}
+
+		private bool convert_order_date()
+		{
+			return DateTime.TryParse(order_date_string, out order_date);
+		}
+
+		private bool convert_due_date()
+		{
+			return DateTime.TryParse(due_date_string, out due_date);
+		}
 	}
 }
--- SKT Due Date Emailer/Program.cs
+++ SKT Due Date Emailer/Program.cs
@@ -19,15 +19,16 @@ namespace SKT_Due_Date_Emailer
 			DateTime userTime = TimeZoneInfo.ConvertTimeFromUtc(today, timeInfo);
 			string date = userTime.Month.ToString().PadLeft(2, '0') + "/" + userTime.Day.ToString().PadLeft(2, '0') + "/" + userTime.Year + " " + userTime.ToString("HH:mm:ss tt");
 
-			List<List<string>> dataListReturn = getAllSKTList();
+			var dataListReturn = getAllSKTList();
+			var filtered_list = filter_list(dataListReturn);
 		}
 
-		static private List<List<string>> getAllSKTList()
+		static private List<socket_entry> getAllSKTList()
 		{
 
-			List<List<string>> dataListReturn = new List<List<string>>();
+			List<socket_entry> dataListReturn = new List<socket_entry>();
 
-			string databaseLocation = "\\\\QA_SERVER\\QA Database\\QUALITY_Database_BE.mdb";
+			string databaseLocation = "\\\\ICEDATA_SERVER\\Log-Book\\BoardDesign_Assy_Database_BE.mdb";
 			OleDbConnection con = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + databaseLocation + ";");
 
 			//Console.Write("Board Inventory Database Location: {0}\n", databaseLocation);
@@ -39,19 +40,19 @@ namespace SKT_Due_Date_Emailer
 
 				if (con.State == ConnectionState.Open)
 				{
-					Console.Write("Quality Database Opened\n");
+					Console.Write("SKT Database Opened\n");
 
 					OleDbCommand cmd = new OleDbCommand();
 					cmd.Connection = con;
 
-					string statement = "SELECT * FROM [tbl_Equipment PM List] WHERE [PM Completed]=FALSE";
+					string statement = "SELECT * FROM [tblSocketTracking] WHERE [SKT - Order Confirmation Date] > #6/14/18#";
 
 
 					cmd.CommandText = statement;
 
 					using (OleDbDataReader rdr = cmd.ExecuteReader())
 					{
-
+						Console.WriteLine("Collecting Entries...");
 						while (rdr.Read())
 						{
 
@@ -59,18 +60,22 @@ namespace SKT_Due_Date_Emailer
 							{
 								//Console.Write("Executing...\n");
 								List<string> tempStringList = new List<string>();
-
-								tempStringList.Add(rdr["System Number"].ToString());
-								tempStringList.Add(rdr["Equipment List Category"].ToString());
-								tempStringList.Add(rdr["Serial No"].ToString());
-								tempStringList.Add(rdr["System Description"].ToString());
-								tempStringList.Add(rdr["PM Cycle (Months)"].ToString());
-								tempStringList.Add(rdr["Last PM Date"].ToString());
-								tempStringList.Add(rdr["Next Due Date"].ToString());
-								tempStringList.Add(rdr["PM Actual Date"].ToString());
+								socket_entry socket = new socket_entry();
+								
+								socket.PO_num = (rdr["SKT - PO #"].ToString());
+								socket.part_num = (rdr["SKT - Part #"].ToString());
+								socket.qty_ordered = (rdr["SKT - Qty Ordered"].ToString()); //quantity
+								socket.order_date_string = (rdr["SKT - PO Date"].ToString());
+								socket.due_date_string = (rdr["SKT - Date Due"].ToString());
+								socket.date_in_string = (rdr["SKT - Date In"].ToString());
+								socket.customer = (rdr["SKT - Customer"].ToString());
+								socket.vendor = (rdr["SKT - Vendor"].ToString());
+								socket.work_order_id = (rdr["SKT - Work Order ID"].ToString());
+								/*
 								tempStringList.Add(rdr["Comments"].ToString());
+								*/
 
-								dataListReturn.Add(tempStringList);
+								dataListReturn.Add(socket);
 
 							}//End if
 
@@ -82,15 +87,81 @@ namespace SKT_Due_Date_Emailer
 				}//End if connection open
 
 				con.Close();
-
+				Console.WriteLine("Connection closed");
 			}
 			catch (Exception err)
 			{
-				Console.Write("ERROR Accessing Quality DB: {0}\n", err.Message);
+				Console.Write("ERROR Accessing BoardDesign_Assy_Database_BE: {0}\n", err.Message);
 			}
 
 			return dataListReturn;
 
 		}//End of getAllPMList
+
+		static private List<socket_entry> filter_list(List<socket_entry> input)
+		{
+			var now = DateTime.Now;
+
+			for (int i = 0; i < input.Count; i++)
+			{
+				var entry = input[i];
+				//populate the datetime field
+				entry.convert_dates(); //converts dates and saves conversion success status
+
+				if (entry.conversion[1])//conversion 2 is order_date, make sure its valid
+				{
+					if (entry.conversion[2]) //if date in exists (its already done)
+					{
+						input.RemoveAt(i);
+						i--;
+					}
+				}
+			}
+
+			return input;
+		}
+	}
+
+	class socket_entry
+	{
+		public string due_date_string;
+		public string order_date_string;
+		public string date_in_string;
+		public string qty_ordered;
+		public string PO_num;
+		public string part_num;
+		public string customer;
+		public string work_order_id;
+		public string vendor;
+
+		public DateTime due_date;
+		public DateTime order_date;
+		public DateTime date_in;
+
+		public bool[] conversion;
+
+		public void convert_dates()
+		{
+			var due_date_bool = convert_due_date();
+			var order_date_bool = convert_order_date();
+			var date_in_bool = convert_date_in();
+
+			conversion = new bool[]{due_date_bool, order_date_bool, date_in_bool};
+		}
+
+		private bool convert_date_in()
+		{
+			return DateTime.TryParse(date_in_string, out date_in);
+		}
+
+		private bool convert_order_date()
+		{
+			return DateTime.TryParse(order_date_string, out order_date);
+		}
+
+		private bool convert_due_date()
+		{
+			return DateTime.TryParse(due_date_string, out due_date);
+		}
 	}
 }
